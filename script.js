08771229294e2eb63f7cd64780439943cbf6689a
filipkk78 <filipkk78@gmail.com>
2024-11-18 22:59:53 +0100

    Change form validation to javascript

--- script.js
+++ script.js
@@ -1,100 +1,91 @@
 const myLibrary = [];
 
 class Book {
-    constructor(title, author, pages, read) {
+  constructor(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    if(read === true) {
-        this.status = "Read";
-    } else if(read === false) {
-        this.status = "Not read";
+    if (read) {
+      this.status = "Read";
+    } else {
+      this.status = "Not read";
     }
     myLibrary.push(this);
-}
-}
-
-const Book1 = new Book ("The Hobbit", "J.R.R. Tolkien", "389", false);
-const Book2 = new Book ("The Great Gatsby", "Scott Fitzgerald", "234", true);
-const Book3 = new Book ("Ulysses", "James Joyce", "132", true);
-const Book4 = new Book ("Pan Tadeusz", "Adam Mickiewicz", "567", false);
-const Book5 = new Book ("1984", "George Orwell", "128", true);
-
-function addBookToLibrary() {
-    let title = document.querySelector("#bookTitle").value;
-    let author = document.querySelector("#bookAuthor").value;
-    let pages = document.querySelector("#bookPages").value;
-    let read;
-    if (document.querySelector("#bookRead").checked == true) {
-    read = true;
-    } else {
-    read = false;
-    }
-    const book = new Book(title, author, pages, read);
-    document.querySelector("#bookTitle").value = '';
-    document.querySelector("#bookAuthor").value = '';
-    document.querySelector("#bookPages").value = '';
-    document.querySelector("#bookRead").checked = false;
-    clearBooks();
-    updateBookDisplay();
+  }
 }
 
-const btn = document.querySelector("#addBook");
-btn.addEventListener('click', addBookToLibrary);
-btn.addEventListener('click', (event) => {
-    event.preventDefault();
+const Book1 = new Book("The Hobbit", "J.R.R. Tolkien", "389", false);
+const Book2 = new Book("The Great Gatsby", "Scott Fitzgerald", "234", true);
+const Book3 = new Book("Ulysses", "James Joyce", "132", true);
+const Book4 = new Book("Pan Tadeusz", "Adam Mickiewicz", "567", false);
+const Book5 = new Book("1984", "George Orwell", "128", true);
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const fd = new FormData(form);
+  const obj = Object.fromEntries(fd);
+  console.log(obj);
+  if (obj.read) {
+    obj.read = true;
+  } else {
+    obj.read = false;
+  }
+  new Book(obj.title, obj.author, obj.pages, obj.read);
+  form.reset();
+  clearBooks();
+  updateBookDisplay();
+  console.log(myLibrary);
 });
 
-const mainContent = document.querySelector(".main-content") ;
+const mainContent = document.querySelector(".main-content");
 
 function updateBookDisplay() {
-    let i = 0;
-    myLibrary.forEach(function(book) {
-        const bookCard = document.createElement("div");
-        bookCard.classList.add("book-card", );
-        bookCard.setAttribute("data-arrayid", i);
-        mainContent.appendChild(bookCard);
-        const titleSpace = document.createElement("h3");
-        titleSpace.textContent = `${book.title}`
-        bookCard.appendChild(titleSpace);
-        const authorSpace = document.createElement("span");
-        authorSpace.textContent = `${book.author}`
-        bookCard.appendChild(authorSpace);
-        const pagesSpace = document.createElement("span");
-        pagesSpace.textContent = `${book.pages}`
-        bookCard.appendChild(pagesSpace);
-        const statusSpace = document.createElement("span");
-        statusSpace.textContent = `${book.status}`
-        bookCard.appendChild(statusSpace);
-        const removeBookBtn = document.createElement("button");
-        removeBookBtn.textContent = "Remove";
-        bookCard.appendChild(removeBookBtn);
-        removeBookBtn.addEventListener("click", function(){
-            const bookIndex = bookCard.dataset.arrayid;
-            myLibrary.splice(bookIndex, 1);
-            clearBooks();
-            updateBookDisplay();
-        })
-        const changeStatusBtn = document.createElement("button");
-        changeStatusBtn.textContent = "Change status";
-        bookCard.appendChild(changeStatusBtn);
-        changeStatusBtn.addEventListener("click", function(){
-            const bookIndex = bookCard.dataset.arrayid;
-            if(myLibrary.at(bookIndex).status == "Read") {
-            myLibrary.at(bookIndex).status = "Not read"
-            } else if(myLibrary.at(bookIndex).status == "Not read") {
-            myLibrary.at(bookIndex).status = "Read"
-            }
-            statusSpace.textContent = `${myLibrary.at(bookIndex).status}`
-        })
-        i++;
-    })
+  let i = 0;
+  myLibrary.forEach((book) => {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+    mainContent.appendChild(bookCard);
+    const titleSpace = document.createElement("h3");
+    titleSpace.textContent = `${book.title}`;
+    bookCard.appendChild(titleSpace);
+    const authorSpace = document.createElement("span");
+    authorSpace.textContent = `${book.author}`;
+    bookCard.appendChild(authorSpace);
+    const pagesSpace = document.createElement("span");
+    pagesSpace.textContent = `${book.pages}`;
+    bookCard.appendChild(pagesSpace);
+    const statusSpace = document.createElement("span");
+    statusSpace.textContent = `${book.status}`;
+    bookCard.appendChild(statusSpace);
+    const removeBookBtn = document.createElement("button");
+    removeBookBtn.textContent = "Remove";
+    bookCard.appendChild(removeBookBtn);
+    removeBookBtn.addEventListener("click", () => {
+      myLibrary.splice(myLibrary.indexOf(book), 1);
+      clearBooks();
+      updateBookDisplay();
+    });
+    const changeStatusBtn = document.createElement("button");
+    changeStatusBtn.textContent = "Change status";
+    bookCard.appendChild(changeStatusBtn);
+    changeStatusBtn.addEventListener("click", function () {
+      const bookIndex = bookCard.dataset.arrayid;
+      if (myLibrary.at(bookIndex).status == "Read") {
+        myLibrary.at(bookIndex).status = "Not read";
+      } else if (myLibrary.at(bookIndex).status == "Not read") {
+        myLibrary.at(bookIndex).status = "Read";
+      }
+      statusSpace.textContent = `${myLibrary.at(bookIndex).status}`;
+    });
+    i++;
+  });
 }
 
 function clearBooks() {
-    while(mainContent.lastElementChild) {
-        mainContent.removeChild(mainContent.lastElementChild);
-    } 
+  while (mainContent.lastElementChild) {
+    mainContent.removeChild(mainContent.lastElementChild);
+  }
 }
 
 updateBookDisplay();
